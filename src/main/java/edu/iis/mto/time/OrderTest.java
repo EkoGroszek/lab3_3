cb6b708f7d38d5fd3f30382a6e89000fdbf3ca2b
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-04-29 01:42:00 +0200

    afterExpirationStatusShouldBeCanceled test added + Refactor

--- src/main/java/edu/iis/mto/time/OrderTest.java
+++ src/main/java/edu/iis/mto/time/OrderTest.java
@@ -6,6 +6,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNot.not;
 
 public class OrderTest {
 
@@ -17,8 +19,7 @@ public class OrderTest {
         orderItem = new OrderItem();
     }
 
-    @Test(expected = OrderExpiredException.class)
-    public void afterExpirationShouldThrowOrderExpiredException() {
+    @Test(expected = OrderExpiredException.class) public void afterExpirationShouldThrowOrderExpiredException() {
         order.addItem(orderItem);
         order.setFakeDate(new DateTime(2019, 4, 1, 0, 0));
         order.submit();
@@ -26,12 +27,35 @@ public class OrderTest {
         order.confirm();
     }
 
-    @Test
-    public void statusShouldBeCreatedAfterAddingNewItem() {
+    @Test public void beforeExpirationStatusShouldNoTBeCancelled() {
+        order.addItem(orderItem);
+        order.setFakeDate(new DateTime(2019, 4, 1, 0, 0));
+        order.submit();
+        order.setFakeDate(new DateTime(2019, 4, 1, 5, 0));
+        order.confirm();
+
+        Assert.assertThat(order.getOrderState(), is(not(equalTo(Order.State.CANCELLED))));
+    }
+
+    @Test public void afterExpirationStatusShouldBeCanceled() {
+        order.addItem(orderItem);
+        order.setFakeDate(new DateTime(2019, 4, 1, 0, 0));
+        order.submit();
+        order.setFakeDate(new DateTime(2019, 4, 30, 0, 0));
+        try {
+            order.confirm();
+        } catch (OrderExpiredException e) {
+
+        }
+        finally {
+            Assert.assertThat(order.getOrderState(), is(Order.State.CANCELLED));
+        }
+    }
+
+    @Test public void statusShouldBeCreatedAfterAddingNewItem() {
         order.addItem(orderItem);
 
         Assert.assertThat(order.getOrderState(), is(Order.State.CREATED));
     }
-
 
 }
